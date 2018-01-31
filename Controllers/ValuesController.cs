73a02714f59ed80291b4067ca683c839db66bac9
idan arazi <idan.arazi@gmail.com>
2018-01-31 21:00:08 +0200

    Initial commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+using Newtonsoft.Json;
+namespace app.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get(string url)
+        {
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            HtmlWeb web = new HtmlWeb();
+            var htmlDoc = web.Load(url);
+            StringBuilder sb = new StringBuilder();
+            try
+            {
+                // Remove script & style nodes
+                htmlDoc.DocumentNode.Descendants().Where(n => n.Name == "script" || n.Name == "style").ToList().ForEach(n => n.Remove());
+                foreach (HtmlNode node in htmlDoc.DocumentNode.SelectNodes("//text()[normalize-space(.) != '']"))
+                {
+                    string[] words = node.InnerText.Trim().Split(' ');
+                    foreach (string word in words)
+                    {
+                        var regexItem = new Regex( @"[0-9@#$%&*+\-_(),+':;?.,![\]\s\\/]+$");
+                        if (!regexItem.IsMatch(word))
+                        {
+                            if (dictionary.ContainsKey(word))
+                            {
+                                dictionary[word] += 1;
+                            }
+                            else
+                            {
+                                dictionary.Add(word, 1);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+            }
+            string json = JsonConvert.SerializeObject(dictionary.OrderByDescending(i => i.Value), Formatting.Indented);
+            return json;
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,32 @@
+﻿function readUrl(){
+    var url = $('#urlInput').val();
+    if (isUrlValid(url)){
+        $.ajax({
+            type: "GET",
+            url: "../api/values",
+            data: { 
+                'url': url
+            }, 
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: successFunc,
+            error: errorFunc
+        });
+
+        function successFunc(data) {     
+            jQuery.each(data, function(i, val) {
+                $("#results").append("<li>the number of occurrences of the word: " + val.Key + " is " + val.Value);
+              });
+        }
+
+        function errorFunc() {
+            alert('There is not a website in that URL');
+        }
+    }
+    else{
+        alert("Please insert a valid URL in the format http://www.example.com");
+    }
+}
+function isUrlValid(url) {
+    return /^(https?|s?ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(url);
+}
